8b416795892ca2aeb58b092e68fd42444df28310
user13691224 <arhuatalipa@gmail.com>
2024-02-25 23:27:52 -0500

    tercer commit

--- 01.py
+++ 01.py
@@ -7,15 +7,13 @@ def factorial(a):
 hola= factorial(0)
 print(hola)
 print("aproximación de seno")
-while True:
-    def sen(a):
-        c=0
-        for i in range(31):
-            c = c + (((a ** (2*i+1))/factorial(2*i+1)) * ((-1) ** i))
-        return c
-    print("Ingrese un número")
-    s=float(input())
-    se=sen(s)
-    print(se)
-    if s==0:
-        break
+
+def sen(a):
+    c=0
+    for i in range(31):
+        c = c + (((a ** (2*i+1))/factorial(2*i+1)) * ((-1) ** i))
+    return c
+print("Ingrese un número")
+s=float(input())
+se=sen(s)
+print(se)
