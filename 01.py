2a733b8aede5e90bdc90bea2dc731c8fb33576b4
user13691224 <arhuatalipa@gmail.com>
2024-02-25 23:18:12 -0500

    segundo cambio...creo

--- 01.py
+++ 01.py
@@ -1,1 +1,21 @@
-print("hola")
+print("holad")
+def factorial(a):
+    b=1
+    for i in range(1,a+1):
+        b*=i
+    return b
+hola= factorial(0)
+print(hola)
+print("aproximación de seno")
+while True:
+    def sen(a):
+        c=0
+        for i in range(31):
+            c = c + (((a ** (2*i+1))/factorial(2*i+1)) * ((-1) ** i))
+        return c
+    print("Ingrese un número")
+    s=float(input())
+    se=sen(s)
+    print(se)
+    if s==0:
+        break
